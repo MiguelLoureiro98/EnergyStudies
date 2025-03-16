c107b7cda39d440eeaa01203cc96b8de828c86b8
MiguelLoureiro98 <miguel.santos.loureiro@gmail.com>
2025-03-16 00:01:40 +0000

    Tested wind turbine simulation.

--- wind/scripts/wind_turbine_sim.py
+++ wind/scripts/wind_turbine_sim.py
@@ -72,12 +72,12 @@ def WT(t: float,
    K_tower = M_tower * (2 * np.pi * 0.3240)**2;
    D_tower = 2 * 0.001 * np.sqrt(K_tower / M_tower);
 
-   eff_wind_speed = wind_speed - np.sqrt(x[3] + x[5]);
+   eff_wind_speed = wind_speed #- np.sqrt(x[3] + x[5]);
    TSR = x[0] * R / eff_wind_speed;
    Cp = Cp_TSR(TSR, x[10]);
    Ct = Ct_TSR(TSR, x[10]);
    rho_air = 1.25;
-   T_aero = np.pi / 2 * rho_air * R**3 * Cp / TSR * eff_wind_speed**2 * np.cos(x[8]);
+   T_aero = np.pi / 2 * rho_air * R**3 * Cp / (TSR + 0.00001) * eff_wind_speed**2 * np.cos(x[8]);
    Ft = np.pi / 2 * rho_air * R**2 * Ct * eff_wind_speed**2;
 
    domega_r = (T_aero - Kdt * x[2] - (Bdt + Br) * x[0] + Bdt / gear_ratio * x[1] - x[12]) / Jr;
@@ -96,15 +96,108 @@ def WT(t: float,
 
    return [domega_r, domega_g, dtheta, dxdot, dx, dydot, dy, dgamadot, dgama, dbetadot, dbeta, dTg, dTHSSB];
 
+def controller(x):
+
+   return [0.0, 0.0, 0.0, 0.0];
+
 if __name__ == "__main__":
+
+   # Plot aerodynamic power and thrust force surfaces.
     
-   tsr = np.arange(0, 30, 0.1);
+   tsr = np.arange(0, 25, 0.1);
    beta = np.arange(-5, 9, 0.1);
    tsr, beta = np.meshgrid(tsr, beta);
-   Ct_surf = Cp_TSR(tsr, beta);
+   Cp_surf = Cp_TSR(tsr, beta);
+   Ct_surf = Ct_TSR(tsr, beta);
 
-   fig, ax = plt.subplots(subplot_kw={"projection": "3d"});
+   fig, ax = plt.subplots(2, 1, subplot_kw={"projection": "3d"});
 
-   ax.plot_surface(tsr, beta, Ct_surf);
+   ax[0].plot_surface(tsr, beta, Cp_surf);
+   ax[0].set_title("Cp (TSR, beta)");
+   ax[0].set_xlabel("TSR");
+   ax[0].set_ylabel("beta");
+   ax[0].set_zlabel("Cp");
+   ax[0].view_init(elev=25, azim=45, roll=0);
+
+   ax[1].plot_surface(tsr, beta, Ct_surf);
+   ax[1].set_title("Ct (TSR, beta)");
+   ax[1].set_xlabel("TSR");
+   ax[1].set_ylabel("beta");
+   ax[1].set_zlabel("Ct");
+   ax[1].view_init(elev=25, azim=45, roll=0);
+
+   plt.show();
+
+   # Initial conditions
+   x0 = np.zeros(13).tolist();
+
+   # Time span
+   t_span = [0, 10];
+   Ts = 0.1;
+   n = int((t_span[1] - t_span[0]) / Ts + 1);
+
+   if(int((t_span[1] - t_span[0]) % Ts) != 0):
+    
+     n += 1;
+    
+   true_time = np.array([np.fmin(t_span[1], t_span[0] + Ts * i) for i in range(n)]);
+
+   results = np.zeros((14, 1));
+
+   for t in range(n-1):
+
+        # Control input
+        beta_ref, T_ref, T_yaw, T_HSSB = controller(x0);
+
+        # Solve ODE
+        sol = solve_ivp(WT, [true_time[t], true_time[t+1]], x0, args=(10, beta_ref, T_ref, T_yaw, T_HSSB));
+
+        # Store results
+        new_data = np.vstack((sol.t, sol.y));
+        results = np.hstack((results, new_data));
+
+        # Update initial conditions
+        x0 = sol.y[:, -1];
+
+   # Plot results
+   fig = plt.figure(figsize=(5, 5));
+
+   ax = fig.subplots(6, 1, sharex=True);
+
+   ax[0].set_title("Wind Turbine response");
+   ax[0].set_xlabel("t (s)");
+   ax[0].set_ylabel("Gen. speed (rad/s)");
+   ax[0].plot(results[0, :], results[2, :] * 60 / (2 * np.pi));
+   ax[0].grid();
+
+   ax[1].set_title("Wind Turbine response");
+   ax[1].set_xlabel("t (s)");
+   ax[1].set_ylabel("Rotor speed (rad/s)");
+   ax[1].plot(results[0, :], results[1, :] * 60 / (2 * np.pi));
+   ax[1].grid();
+
+   ax[2].set_title("Wind Turbine response");
+   ax[2].set_xlabel("t (s)");
+   ax[2].set_ylabel("Torsion angle (rad)");
+   ax[2].plot(results[0, :], results[3, :] / (2 * np.pi));
+   ax[2].grid();
+
+   ax[3].set_title("Wind Turbine response");
+   ax[3].set_xlabel("t (s)");
+   ax[3].set_ylabel("Beta (deg)");
+   ax[3].plot(results[0, :], results[11, :]);
+   ax[3].grid();
+
+   ax[4].set_title("Wind Turbine response");
+   ax[4].set_xlabel("t (s)");
+   ax[4].set_ylabel("Gama (deg)");
+   ax[4].plot(results[0, :], results[9, :]);
+   ax[4].grid();
+
+   ax[5].set_title("Wind Turbine response");
+   ax[5].set_xlabel("t (s)");
+   ax[5].set_ylabel("x_dot (m/s)");
+   ax[5].plot(results[0, :], results[5, :]);
+   ax[5].grid();
 
    plt.show();
